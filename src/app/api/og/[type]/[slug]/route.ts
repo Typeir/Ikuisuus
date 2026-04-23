76629a869ba7547261d507265f46203fabe24d95
Typeir <david.t.leiro@gmail.com>
2026-04-23 21:49:22 +0200

    [dirty]: png as any

--- src/app/api/og/[type]/[slug]/route.ts
+++ src/app/api/og/[type]/[slug]/route.ts
@@ -112,7 +112,7 @@ export async function GET(
       backgroundImageUrl,
       description: data.description,
     });
-    return new Response(png, {
+    return new Response(png as any, {
       headers: {
         'Content-Type': 'image/png',
         'Cache-Control': CACHE_CONTROL,
